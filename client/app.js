f3bdeda675edc39f2e6d12766f43f0c0dd76e5cb
Daniel Dvorkin <Dvorkin212@gmail.com>
2016-05-24 19:32:36 -0400

    Start Work: made some changes to the method that produces the author name

--- client/app.js
+++ client/app.js
@@ -1,8 +1,8 @@
 Template.registerHelper("usernameFromId", function (userId){
 	var user = Meteor.users.findOne({_id: userId});
-	if(user == null){
-		return $('p').html('<a href="/login">Login</a> to view author name');
-	} else {
-		return user.username;
+	console.log(user);
+	if(typeof user === "undefined"){
+		return "";
 	}
+	return user.username;
 });
