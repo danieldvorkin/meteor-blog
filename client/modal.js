414f1e0738820e6cbab9c85f5c9537f8f5a2ff93
Daniel Dvorkin <dvorkin212@gmail.com>
2016-05-27 11:28:55 -0400

    Got the update modal to work perfectly with update

--- client/modal.js
+++ client/modal.js
@@ -1,10 +1,10 @@
 Template.modal.events({
 	'submit #updatePost': function(e){
 		e.preventDefault();
+		var post = Posts.findOne({title: this.title, description: this.desc});
 		var title = $('#blog-title').val();
 		var desc = $('#blog-value').val();
-		var post = Posts.findOne({title: title, description: desc});
-			
+
 		Meteor.call('updatePost', post._id, title, desc, function(error){
 			if(error){
 				alert(error);
@@ -12,5 +12,6 @@ Template.modal.events({
 				console.log("awesome update u guys");
 			}
 		});
+		$('#myModal').modal('hide');
 	}
 })
